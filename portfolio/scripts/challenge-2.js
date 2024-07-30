e0909e9e77a6bc1a152c48e1e39e6b33bf1656fe
Abdelfatah Elsayed <153190831+abdelfatah-dev@users.noreply.github.com>
2024-07-30 05:15:30 +0300

    Add files via upload

---
+++ portfolio/scripts/challenge-2.js
@@ -0,0 +1,76 @@
+const skillsArray = [
+  {
+    name: 'HTML',
+    experience: '2023',
+    type: 'frontend',
+  },
+  {
+    name: 'CSS',
+    experience: '2023',
+    type: 'frontend',
+  },
+  {
+    name: 'Javascript',
+    experience: '2023',
+    type: 'frontend',
+  },
+  {
+    name: 'React',
+    experience: '2023',
+    type: 'frontend',
+  },
+  {
+    name: 'Redux',
+    experience: '2024',
+    type: 'frontend',
+  },
+  {
+    name: 'React Testing Library',
+    experience: '2024',
+    type: 'frontend',
+  },
+  {
+    name: 'Node.js',
+    experience: '2024',
+    type: 'bakend',
+  },
+  {
+    name: 'Express.js',
+    experience: '2024',
+    type: 'backend',
+  },
+  {
+    name: 'Mongodb',
+    experience: '2024',
+    type: 'backend',
+  },
+];
+
+// Add skills dynamically
+
+const skillsElement = document.querySelector('#skills');
+
+const currentYear = new Date().getFullYear();
+
+console.log(currentYear);
+
+const generateListItem = (listItem) => `<li class="skills__column" data-type=${listItem.type} data-name=${listItem.name}>
+<h2 class="skills__title">${listItem.name}</h2>
+<span class="skills__years">${currentYear - listItem.experience} Years</span>
+</li>`;
+
+const listItemsString = skillsArray
+  .map((listItem) => generateListItem(listItem))
+  .join('');
+
+const unorderdList = `<ul class="skills__list">${listItemsString}</ul>`;
+
+skillsElement?.insertAdjacentHTML('beforeend', unorderdList);
+
+document.querySelectorAll('.skills__button').forEach((element) => {
+  element.addEventListener('click', () => {
+    document.querySelector('.skills__button--isActive').classList.remove('skills__button--isActive'); 
+    element.classList.add('skills__button--isActive');
+    document.querySelector('.skills__list').classList = `skills__list ${element.dataset.type}`;
+  });
+});
